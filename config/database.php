d740e2161bbecf21bb2a4db79344bada8cfee83d
Marvellous Obor <marveljae731@gmail.com>
2026-07-27 10:00:08 +0100

    api config for deploy

--- config/database.php
+++ config/database.php
@@ -5,9 +5,9 @@
  */
 
 return [
-    'host'     => 'localhost',
-    'user'     => 'root',
-    'password' => '',
-    'database' => 'idat_academy',
+    'host'     => 'sql211.infinityfree.com',
+    'user'     => 'if0_42428040',
+    'password' => 'Aplx20014',
+    'database' => 'if0_42428040_idatacademy',
     'charset'  => 'utf8mb4',
 ];
